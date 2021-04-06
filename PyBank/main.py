a386f2e38c4db0773605708fb0b309a4c599545e
Jamin Hershberger <jaminhershberger@Jamins-MBP.lan1>
2021-04-06 08:29:09 -0400

    this is adding my TEXt File to the PyBank part of the homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,25 +1,19 @@
 import os
 import csv
 
-# 2 list's to represent my columns
-
-
 # Path to collect data from the Resources folder
 dir_path = os.path.dirname(os.path.realpath(__file__))
 #print(dir_path)
-
 os.chdir(dir_path)
 budget_csv = os.path.join('Resources','budget_data.csv')
-
 #start to read my CSV file
-
 with open(budget_csv, 'r',encoding="utf-8") as csvfile:
 
         # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     
     csv_header=next(csvreader)
-    print(f"header:{csv_header}")
+    #print(f"header:{csv_header}")
     # created lists to store column data
     month = []
     revenue = []
@@ -30,82 +24,79 @@ with open(budget_csv, 'r',encoding="utf-8") as csvfile:
     for row in csvreader:
         month.append(row[0])
         revenue.append(row[1])
-    print(len(month)
-# to collect total revenue
+    #print(f'total months:{len(month)}')
+
+#to collect total revenue over all months
+    revenue_total =map(int,revenue)
+    total_revenue = (sum(revenue_total))
+    #print(f'total:{total_revenue}')
+
+# to calculate average change
+    x = 0
+    for x in range(len(revenue) -1):
+        profit_loss = int(revenue[x+1]) -int(revenue[x])
+        revenue_change.append(profit_loss)
+    total=sum(revenue_change)
+
+#find monthly changes
+    monthly_change = round (total / len(revenue_change),2)
+
+#to print average changes
+    #print(f"average change: {monthly_change}")
+
+#to find the greatest increase
+    profit_increase = max(revenue_change)
+    #print(profit_increase)
+    H = revenue_change.index(profit_increase)
+    month_increase =month[H+1]
+    #print(f"Greatest Increase in Profits: {month_increase} (${profit_increase})")
+
+#to find the greatest decrease
+    profit_decrease = min(revenue_change)
+    #print(profit_decrease)
+    I = revenue_change.index(profit_decrease)
+    month_decrease = month[I+1]
+    #print(f"Greatest Decrease in Profits: {month_decrease} (${profit_decrease})")
+# to print to a TEXT file
+    textoutput = os.path.join('..', 'PyBank', 'budget_summary.txt')
+with open (textoutput, 'w', newline='') as budget:
+    write = csv.writer(budget)
+    write.writerows([
+            ["Financial Analysis for budget_data.csv"],
+            ["-------------------------------------"],
+            ["Total number of months: " + str(len(month))],
+            ["Total Revenue in period: $ " + str(total_revenue)],
+            ["Average monthly change in Revenue : $" + str(monthly_change)],
+            ["Greatest Increase in Profits:" +str(month_increase) + "($" +str(profit_increase) +")"],
+            ["Greatest Decrease in Profits:" +str(month_decrease) + "($" +str(profit_decrease) +")"]
+    ])
+
+
+#all the print statements together now
+    print(f'Financial Analysis'+'\n')
+    print(f'----------------------------'+'\n')
+
+
+    print("Total number of months: " + str(len(month)))
+
+    print("Total Revenue in period: $ " + str(total_revenue))
+      
+    print("Average monthly change in Revenue : $" + str(monthly_change))
+
+    print(f"Greatest Increase in Profits: {month_increase} (${profit_increase})")
+
+    print(f"Greatest Decrease in Profits: {month_decrease} (${profit_decrease})")
   
-
-
-
+     
     
-    #months = []
-    #net_profit_loss = []
-    
-    #for row in csvreader:
-        #total_months.append(row[0])
-        
-        #net_profit_loss.append(row[1])
-        
-        #row_count = sum(1 for row[0] in csvreader)
-
-        #average =0
-        #average = average(net_profit_loss)
-        #net_profit_loss.append(average)
-        #print (f'the total months in this data set is:  {row_count}')
-        #print(net_profit_loss)
-        
-        #for row in csvreader:
-            #print(row)
-            #total =0
-            #total += int(row[1])
-            #net_profit_loss.append(total)
-
-        
-        #print(row)
-        
-        
-        #row_count = sum(1 for row in csvreader)
-
-        
-        
-
-        #total =0
-        #total += range(row[1]
-        
-        
-        #profit_loss.append(row[1])
-        
-        #total =0
-        #total += range(row[1]
-        
-        
-        
-        
-        #print(total)
-
-        
-
-    #print(analysis(month_year))
-
-   
-        # Loop through the data
-    #for row in csvreader:
-        #month_year = str(row[1])
-        #profit_loss = int(table_data[1])
-        #row_count = sum(1 for row in csvreader)
-        #sum =range(int([1]))
-        #total = sum(int([1]))
-        #profit_loss += for row[1] in csvreader
-        #print(f'the total months in this data set is:  {row_count}')
-        
-        #print(table_data[0])
-
-    
-
-    
-
-       
-
-
-
-
-
+     #print(type(row))
+        # this will print months
+        #print row[1]
+# this will print profit, and loss = sum this
+        #print(row[1])
+        #changes in profit,and losses, and find the the average of the changes
+        # find maximum (Date included)
+        #find minimum  (date included)
+        #print
+        #create txt file
+        #write to the txt file
